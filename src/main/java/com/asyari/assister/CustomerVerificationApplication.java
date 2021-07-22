4e07463aa67faa4075e60c25c32b60d752f17029
20058737 <ilham.asyari@btpn.com>
2021-07-22 21:36:23 +0700

    Initial commit

---
+++ src/main/java/com/asyari/assister/CustomerVerificationApplication.java
@@ -0,0 +1,13 @@
+package com.asyari.assister;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerVerificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerVerificationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/asyari/assister/CustomerVerificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asyari.assister;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerVerificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
